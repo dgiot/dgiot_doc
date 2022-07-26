7833ac769a64443f480975ae303abd89f0727511
h7ml <h7ml@qq.com>
2022-07-26 17:47:38 +0800

    首页导航

--- config/presets.js
+++ config/presets.js
@@ -1,36 +1,39 @@
 module.exports = [
-    [
-        'classic',
-        /** @type {import('@docusaurus/preset-classic').Options} */
-        ({
-            debug: true,
-            sitemap: {
-                changefreq: 'weekly',
-                priority: 0.5,
-                ignorePatterns: ['/tags/**'],
-                filename: 'sitemap.xml',
-            },
-            gtag: {
-                trackingID: 'G-226F0LR9KE',
-                anonymizeIP: true,
-            },
-            googleAnalytics: {
-                trackingID: 'UA-141789564-1',
-                anonymizeIP: true,
-            },
-            docs: {
-                sidebarPath: require.resolve('./sidebars.js'),
-                // Please change this to your repo.
-                editUrl: 'https://github.com/dgiot/dgiot_doc/tree/main/',
-            },
-            blog: {
-                showReadingTime: true,
-                // Please change this to your repo.
-                editUrl: 'https://github.com/dgiot/dgiot_doc/tree/main/',
-            },
-            theme: {
-                customCss: require.resolve('../src/css/custom.css'),
-            },
-        }),
-    ],
+  [
+    'classic',
+    /** @type {import('@docusaurus/preset-classic').Options} */
+    ({
+      debug: true,
+      sitemap: {
+        changefreq: 'weekly',
+        priority: 0.5,
+        ignorePatterns: ['/tags/**'],
+        filename: 'sitemap.xml',
+      },
+      gtag: {
+        trackingID: 'G-226F0LR9KE',
+        anonymizeIP: true,
+      },
+      googleAnalytics: {
+        trackingID: 'UA-141789564-1',
+        anonymizeIP: true,
+      },
+      docs: {
+        sidebarPath: require.resolve('./sidebars.js'),
+        // Please change this to your repo.
+        editUrl: 'https://github.com/dgiot/dgiot_doc/tree/main/',
+      },
+      blog: {
+        showReadingTime: true,
+        // Please change this to your repo.
+        editUrl: 'https://github.com/dgiot/dgiot_doc/tree/main/',
+        // feedOptions: {
+        //   type: 'all'
+        // },
+      },
+      theme: {
+        customCss: require.resolve('../src/css/custom.css'),
+      },
+    }),
+  ],
 ]
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,30 +1,111 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
-
-const FeatureList =[];
-
-function Feature({Svg, title, description, viewBoxVal}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" viewBox={viewBoxVal} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+import React from 'react'
+import styles from './styles.module.css'
+import { Col, Row, Card, Button, Divider } from 'antd'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+// 获取路由导航
+// 1. 打开 https://doc.dgiotcloud.cn/docs/product_doc/docs/deployment_details/system_deployment/
+// 2. 获取父节点的数据
+// todo: 需要获取节点路径并在link中添加
+// let nav = []
+// document
+//   .querySelectorAll('.theme-doc-sidebar-item-category-level-1')
+//   .forEach((i, index) => {
+//     nav.push({
+//       text: i
+//         .getElementsByClassName('menu__list-item-collapsible')[0]
+//         .querySelectorAll('.menu__link--sublist')[0].text,
+//     })
+//     nav[index].children = []
+//     i.querySelectorAll('.menu__link--sublist-caret').forEach((t) => {
+//       nav[index].children.push({ text: t.text })
+//     })
+//     console.log(i.getElementsByClassName('menu__list-item-collapsible'))
+//   })
+// console.log(nav)
+// 3. 拷贝节点数据
+// window.copy(JSON.stringify(nav))
+function HomepageHeader() {
+  // 下面打印函数可以拿到打印的path路径。但是无法拿到title，因为title是在组件中定义的。
+  // console.log(useDocusaurusContext().globalData['docusaurus-plugin-content-docs'].default.versions[0].docs)
+  // todo: 需要写link地址
+  const nav = [
+    {
+      text: '用户手册',
+      children: [
+        { text: '设备运维' },
+        { text: '智慧园区' },
+        { text: '数字工厂' },
+        { text: '云检测' },
+        { text: '云压测' },
+      ],
+    },
+    {
+      text: '产品文档',
+      children: [
+        { text: '部署详情' },
+        { text: '简介' },
+        { text: '设备云' },
+        { text: '多租户' },
+        { text: '云运维' },
+        { text: '云函数' },
+        { text: '云系统' },
+        { text: '云检测' },
+        { text: '云日志' },
+      ],
+    },
+    {
+      text: '开发指南',
+      children: [
+        { text: '系统架构' },
+        { text: '后端技术' },
+        { text: '数据标识' },
+        { text: '前端技术' },
+        { text: '时序数据' },
+      ],
+    },
+    {
+      text: '实战教程',
+      children: [
+        { text: '系统架构' },
+        { text: '后端技术' },
+        { text: '数据标识' },
+        { text: '前端技术' },
+        { text: '时序数据' },
+      ],
+    },
+  ]
+  // 维护时,只需维护该nav的数据
+  return nav.map((item, index) => (
+    <Card
+      style={{ marginBottom: '20px' }}
+      hoverable={true}
+      key={index}
+      xs={12}
+      sm={6}
+    >
+      {/*todo:循环中需要写src地址跳转*/}
+      <Row>
+        <Col span={2}>
+          <Button type="primary">{item.text}</Button>
+        </Col>
+        <Col span={20} offset={1} style={{ borderLeft: '1px solid #666' }}>
+          {/*循环item.children*/}
+          {item.children.map((i, index) => (
+            <Button type="link" key={index}>
+              {i.text}
+            </Button>
+          ))}
+        </Col>
+      </Row>
+    </Card>
+  ))
 }
-
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
-        </div>
+        <HomepageHeader />
       </div>
     </section>
-  );
+  )
 }
