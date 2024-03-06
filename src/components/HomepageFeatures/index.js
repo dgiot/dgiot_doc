792be5a18630cc80222dbca7d843bdffc08d8649
dawnwinterLiu <1737801684@qq.com>
2024-03-06 09:49:31 +0800

    feat: doc index

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import styles from './styles.module.css'
-import { Col, Row, Card, Button, Divider } from 'antd'
+import {
+	Col,
+	Row,
+	Card,
+	Button,
+	Divider
+} from 'antd'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 // 获取路由导航
 // 1. 打开 https://doc.dgiotcloud.cn/docs/product_doc/docs/deployment_details/index/
@@ -25,94 +31,125 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 // 3. 拷贝节点数据
 // window.copy(JSON.stringify(nav))
 function HomepageHeader() {
-  // 下面打印函数可以拿到打印的path路径。但是无法拿到title，因为title是在组件中定义的。
-  // console.log(useDocusaurusContext().globalData['docusaurus-plugin-content-docs'].default.versions[0].docs)
-  // todo: 需要写link地址
-  const nav = [
-    {
-      text: '用户手册',
-      children: [
-        { text: '设备运维',href:"/docs/user_manual/docs/equipment_operational/equipment_leasing" },
-        { text: '智慧园区' },
-        { text: '数字工厂' },
-        { text: '云检测' },
-        { text: '云压测' },
-      ],
-    },
-    {
-      text: '产品文档',
-      children: [
-        { text: '部署详情' },
-        { text: '简介' },
-        { text: '设备云' },
-        { text: '多租户' },
-        { text: '云运维' },
-        { text: '云函数' },
-        { text: '云系统' },
-        { text: '云检测' },
-        { text: '云日志' },
-      ],
-    },
-    {
-      text: '开发指南',
-      children: [
-        { text: '系统架构' },
-        { text: '后端技术' },
-        { text: '数据标识' },
-        { text: '前端技术' },
-        { text: '时序数据' },
-      ],
-    },
-    {
-      text: '实战教程',
-      children: [
-        { text: '基础知识' },
-        { text: '电表接入' },
-        { text: '视频教程' },
-        { text: 'PT100传感器接入教程' },
-        { text: '电力集抄教程' },
-        { text: 'DDZY1296电表接入教程' },
-        { text: '三合一传感器接入教程' },
-        { text: 'Mqtt设备接入教程' },
-        { text: '水泵性能曲线测量案例' },
-        { text: 'OPC-DA设备接入教程' },
-        { text: '摄像头接入' },
-        { text: '测试' },
-      ],
-    },
-  ]
-  // 维护时,只需维护该nav的数据
-  return nav.map((item, index) => (
-    <Card
-      style={{ marginBottom: '20px' }}
-      hoverable={true}
-      key={index}
-      xs={12}
-      sm={6}
-    >
-      {/*todo:循环中需要写src地址跳转*/}
-      <Row>
-        <Col span={2}>
-          <Button type="primary">{item.text}</Button>
-        </Col>
-        <Col span={20} offset={1} style={{ borderLeft: '1px solid #666' }}>
-          {/*循环item.children*/}
-          {item.children.map((i, index) => (
-            <Button type="link" href={i.href} key={index}>
-              {i.text}
-            </Button>
-          ))}
-        </Col>
-      </Row>
-    </Card>
-  ))
+	// 下面打印函数可以拿到打印的path路径。但是无法拿到title，因为title是在组件中定义的。
+	// console.log(useDocusaurusContext().globalData['docusaurus-plugin-content-docs'].default.versions[0].docs)
+	// todo: 需要写link地址
+	const nav = [{
+			children: [
+			   {
+					"text": "设备运维",
+					"content": "工业设备租赁一体化运维平台以数据为基础、以算法为支撑、以场景为导向，应用先进的实时大数据处理。",
+					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+				},
+				{
+					"text": "智慧园区",
+					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+				},
+				{
+					"text": "数字工厂",
+					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+				},
+				{
+					"text": "云检测",
+					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+				},
+				{
+                					"text": "设备运维",
+                					"content": "工业设备租赁一体化运维平台以数据为基础、以算法为支撑、以场景为导向，应用先进的实时大数据处理。",
+                					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+                					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+                				},
+                				{
+                					"text": "智慧园区",
+                					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+                					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+                				},
+                				{
+                					"text": "数字工厂",
+                					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+                					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+                				},
+                				{
+                					"text": "云检测",
+                					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+                					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+                				},
+                				{
+                                					"text": "设备运维",
+                                					"content": "工业设备租赁一体化运维平台以数据为基础、以算法为支撑、以场景为导向，应用先进的实时大数据处理。",
+                                					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+                                					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+                                				},
+                                				{
+                                					"text": "智慧园区",
+                                					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+                                					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+                                				},
+                                				{
+                                					"text": "数字工厂",
+                                					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+                                					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+                                				},
+                                				{
+                                					"text": "云检测",
+                                					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+                                					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+                                				},
+                                				{
+                                                					"text": "设备运维",
+                                                					"content": "工业设备租赁一体化运维平台以数据为基础、以算法为支撑、以场景为导向，应用先进的实时大数据处理。",
+                                                					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+                                                					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+                                                				},
+                                                				{
+                                                					"text": "智慧园区",
+                                                					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+                                                					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+                                                				},
+                                                				{
+                                                					"text": "数字工厂",
+                                                					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+                                                					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+                                                				},
+                                                				{
+                                                					"text": "云检测",
+                                                					"cover": "http://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/dgiot_web/Community.png",
+                                                					"href": "/docs/user_manual/docs/equipment_operational/equipment_leasing"
+                                                				}
+			]
+		}
+
+	]
+	// 维护时,只需维护该nav的数据
+	return nav.map((item, index) => (
+		<div style={{ display: "flex", flexWrap: "wrap" }}>
+       {item.children.map((i, index) => (
+         <Col key= {{index}} span={5} offset={1} style={{ border: '0px solid #666' }}>
+           <a href={i.href} >
+              <Card
+               hoverable={true}
+               key={index}
+               style={{border: '0px solid #666', margin: '0px 0px 10px 0px'}}
+               >
+               <img alt="cover" href={i.href} src={i.cover} style={{height: '80%',width: '80%',objectFit: 'cover'}} />
+              </Card>
+           </a>
+         </Col>
+       ))}
+    </div>
+	))
 }
+
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
+	return (
+		<section className={styles.features}>
       <div className="container">
         <HomepageHeader />
       </div>
     </section>
-  )
+	)
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,31 +4,46 @@ import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Translate, {translate} from '@docusaurus/Translate';
+import Translate, {
+	translate
+} from '@docusaurus/Translate';
 
 function HomepageHeader() {
-    const {siteConfig} = useDocusaurusContext();
-    return (
-        <header className={clsx("hero hero--primary", styles.heroBanner)}>
-            <div className="container">
-                <h1 className="hero__title">{siteConfig.title}</h1>
-                <span className="indexCtasGitHubButtonWrapper_AOht">
-        <img src="https://gitee.com/dgiiot/dgiot/badge/star.svg?theme=gvp" alt="gvp"/>
-        </span>
-                <p></p>
-                <p className="hero__subtitle">
-                    <Translate>Dgiot helps developers quickly connect subsystems to achieve data aggregation with a few
-                        lines of code, and can also achieve common device access with zero code</Translate>
-                </p>
+	const {
+		siteConfig
+	} = useDocusaurusContext();
+	return (
+		<header className={styles.heroBanner,styles.hero}>
+            <div className={styles.container}>
+                <img src="https://dgiot-1253666439.cos.ap-shanghai-fsi.myqcloud.com/atom/djs.png"/>
+            </div>
+
+            <div className={styles.container1}>
+                            <h1 className={styles.hero1}>DGIOT</h1>
+                            <h1 className={styles.hero2}>让万物互联变得更简单</h1>
+                            <span className="indexCtasGitHubButtonWrapper_AOht">
+                    <img src="https://gitee.com/dgiiot/dgiot/badge/star.svg?theme=gvp" alt="gvp"/>
+                    </span>
+                            <p></p>
+                            <p className="hero__subtitle">
+                            <a  href="https://doc.dgiotcloud.cn/docs/product_doc/docs/deployment_details/">
+                              <button className={styles.buttons1}>进入文档 →</button>
+                            </a>
+                            <a  href="https://prod.dgiotcloud.cn/#/login">
+                              <button className={styles.buttons2}>进入控制台</button>
+                            </a>
+                            </p>
             </div>
         </header>
-    );
+	);
 }
 
 export default function Home() {
-    const {siteConfig} = useDocusaurusContext();
-    return (
-        <Layout
+	const {
+		siteConfig
+	} = useDocusaurusContext();
+	return (
+		<Layout
             title={`${siteConfig.title}`}
             description="DGIOT让万物互联变得更简单 <head />"
         >
@@ -37,6 +52,5 @@ export default function Home() {
                 <HomepageFeatures/>
             </main>
         </Layout>
-    );
+	);
 }
-
